660d30d98ef211b5a7e80b1ea7b43e4724297bc2
Andrew <andrewriznykua93@gmail.com>
2020-04-10 20:09:34 -0500

    patch route for ticket added

--- src/routes/ticketRoutes.ts
+++ src/routes/ticketRoutes.ts
@@ -8,6 +8,7 @@ router
   .get('/tickets', verifyToken, Ticket.prototype.getAllTickets)
   .post('/tickets', verifyToken, Ticket.prototype.addTicket)
   .patch('/tickets/:id', verifyToken, Ticket.prototype.editTicket)
+  .patch('/ticket/:id', verifyToken, Ticket.prototype.closeTicket)
   .delete('/tickets/:id', verifyToken, Ticket.prototype.deleteTicket)
 
 export default router;
